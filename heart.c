927e90a0dfce88ce1f099de064b1f4461a38fcca
drawde600 <drawde600@gmail.com>
2019-01-18 14:45:47 +0800

    Update heart.c

--- heart.c
+++ heart.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
 #include<string.h>
 
+//derived from the equation
+//http://mathworld.wolfram.com/HeartCurve.html
+//(x^2 + y^2 - 1)^3 - x^2 y^3 = 0
+  
 #define SQUARE(x) ((x)*(x))
 #define CUBE(x) ((x)*(x)*(x))
 
